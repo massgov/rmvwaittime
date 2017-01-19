290a825a745d8bfd7604e778a3d8b3b79c047c3f
Jes Constantine <jesconstantine@gmail.com>
2017-01-19 17:11:19 -0500

    Organize waittime fetcher code, use moment js for parsing

--- js/waittime-fetcher.js
+++ js/waittime-fetcher.js
@@ -1,75 +1,195 @@
 /**
- * Created by jconstantine on 1/16/17.
+ * @file
+ * RMV Wait Time Component Script
+ * Gets, transforms, and renders the wait times for a specific rmv branch.
+ * See ticket: https://jira.state.ma.us/browse/DP-822
  */
-// https://jira.state.ma.us/browse/DP-822
+
+/**
+ * ES6 shims
+ */
+
+// String.startsWith()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
+if (!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position){
+    position = position || 0;
+    return this.substr(position, searchString.length) === searchString;
+  };
+}
+
+/**
+ * Main Wait Time component script.
+ */
 (function(){
   'use strict';
-  // API URL
+
+  // Cache the wait time container selector.
+  var el = $('.ma__wait-time');
+
+  // Cache the API URL.
   // var rmvWaitTimeURL = 'https://www.massdot.state.ma.us/feeds/qmaticxml/qmaticXML.aspx';
-  var rmvWaitTimeURL = 'waittime.xml';
+  var rmvWaitTimeURL = 'waittime.xml'; // local stub
 
+  // @todo Get branch css path iframe src URL (passed by drupal content authors) -- url parser helper in Mayflower
+  // @todo Get branch js path iframe src URL (passed by drupal content authors) -- url parser helper in Mayflower
 
-  // Get branch css path iframe src URL (passed by drupal content authors) -- url parser helper in Mayflower
-  // Get branch js path iframe src URL (passed by drupal content authors) -- url parser helper in Mayflower
+  /**
+   * Get the current time to show when latest wait times were updated.
+   * @function
+   * @returns {String.} String of the current time (5:48 PM).
+   * @todo move this functionality into a mayflower helper?
+   */
+  var getCurrentTime = function() {
+    var now = new Date(),
+      hours = now.getHours(),
+      minutes = now.getMinutes();
 
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
 
-  // Capture current time to show when last refresh occurred
-  // for: Wait times refreshed at ______ in component footer
-  var currentTimestamp = Date.now();
+    return hours + ':' + minutes + ' ' + ampm;
+  };
 
-  // Get branch from iframe src URL (passed by drupal content authors)
+  /**
+   * Render the transformed wait times for the requested branch on the page.
+   * @function
+   * @param {Object} data branch display data with processed wait times.
+   */
+  var render = function(data) {
+    var $licensing = $('span[data-variable="licensing"]');
+    $licensing.text(data.processedLicensing);
+
+    var $registration = $('span[data-variable="registration"]');
+    $registration.text(data.processedRegistration);
+
+    var $timestamp = $('span[data-variable="timestamp"]');
+    var time = getCurrentTime();
+    $timestamp.text(time);
+  };
+
+  /**
+   * Parse the URL querystring parameters.
+   * @function
+   * @returns {Array.} Array of the querystring parameter keys.
+   * @todo move this functionality into a mayflower helper
+   */
+  var parseParamsFromUrl = function () {
+    var params = {};
+    var parts = window.location.search.substr(1).split('&');
+    for (var i = 0; i < parts.length; i++) {
+      var keyValuePair = parts[i].split('=');
+      var key = decodeURIComponent(keyValuePair[0]);
+      params[key] = keyValuePair[1] ?
+        decodeURIComponent(keyValuePair[1].replace(/\+/g, ' ')) :
+        keyValuePair[1];
+    }
+    return params;
+  };
+
+  /**
+   * Get branch town value.
+   * @function
+   * @returns {String.} The name of the town passed to the script.
+   */
   function getLocation() {
-    // return pretendTheresAQueryParamFn('loc');
-    // return 'Brockton';
-    // return 'Natick'; // closed
-    // return 'Milford'; // No wait time
-    return 'Lowell'; // Less than a minute
+    var urlParams = parseParamsFromUrl();
+
+    // Define param for the branch town query.
+    var branchParamName = 'town';
+
+    if (urlParams[branchParamName]) {
+      return urlParams[branchParamName];
+    }
   }
 
+  /**
+   * Transform the wait time raw strings into processed wait time strings according to ticket spec.
+   * @function
+   * @param {String} waitTime is the raw wait time for either the branch licensing or registration.
+   * @returns {String.} A transformed wait time duration in human readable format.
+   *
+   * Specs from ticket (see link at top of file):
+   * Closed = Closed
+   * 00:00:00 = No wait time
+   * < 1 minute = Less than a minute
+   * if hours == 1 ... string += 1 hour
+   * if hours > 1 string += [0] hours
+   *  >> if [1], string += [1] round up to quarter hour minutes when not = 0
+   * singular minute/hour for 1, plural for +1 (no abbreviations)
+   * < 1 hour: round to the minute
+   * > 1 hour: round to the quarter hour
+   */
+  function transformTime(waitTime) {
+    // Default to unavailable.
+    var displayTime = 'Estimation unavailable';
 
-  // processWaittimes()
-  var processWaittimes = function (branch) {
-    branch.processedLicensing = 'Estimation unavailable';
-    branch.processedLicensing = 'Estimation unavailable';
+    // Closed = 'Closed'.
+    if (waitTime == 'Closed') {
+      displayTime = 'Closed';
+    }
+    // 0 = 'No wait time'.
+    else if (waitTime == '00:00:00') {
+      displayTime = 'No wait time';
+    }
+    // < 1 minute = 'Less than a minute'.
+    else if (waitTime.startsWith('00:00:')) {
+      displayTime = 'Less than a minute';
+    }
+    // Everything else.
+    else {
+      // Create locale with widget time config.
+      moment.updateLocale('rmv', {
+        relativeTime : {
+          s:  "Less than a minute",
+          m:  "1 minute",
+          mm: "%d minutes",
+          h:  "1 hour",
+          hh: "%d hours"
+        },
+        relativeTimeThreshold : {
+          s:  10,
+          m:  30,
+          h:  20
+        }
+      });
 
-    // Closed = 'Closed'
-    branch.processedLicensing = branch.licensing == 'Closed' ? 'Closed' : branch.procssedLicensing;
-    branch.processedRegistration = branch.registration == 'Closed' ? 'Closed' : branch.procssedRegistration;
+      // @todo troubleshoot seconds rounding minute down
+      // @todo figure out rounding so that minutes are still shown for 1+ hour
+      // Use newly created locale.
+      moment.locale('rmv');
 
-    // 0 = 'No wait time'
-    branch.processedLicensing = branch.licensing == '00:00:00' ? 'No wait time' : branch.procssedLicensing;
-    branch.processedRegistration = branch.registration == '00:00:00' ? 'No wait time' : branch.procssedRegistration;
+      displayTime = moment.duration(waitTime).humanize();
+    }
 
+    return displayTime;
+  }
 
-    // < 1 minute = 'Less than a minute'
-    branch.processedLicensing = branch.licensing.includes('00:00:') ? 'Less than a minute' : branch.procssedLicensing;
-    branch.processedRegistration = branch.registration.includes('00:00:') ? 'Less than a minute' : branch.procssedRegistration;
+  /**
+   * Pass the wait time raw strings into function to transform wait time strings according to ticket spec.
+   * @function
+   * @param {Object} branch contains wait time strings for licensing and registration.
+   * @returns {Object.} An object of the requested branch with additional processed wait time stings for 'licensing'
+   * and 'registration'.
+   */
+  function processWaitTimes(branch) {
 
-    // explode string into array
-    var licensingArray = branch.licensing.split(":");
-    var registrationArray = branch.registration.split(":");
-
-    // if [0] == 1 string += 1 hour
-    // if [0] > 1 string += [0] hours
-    // >> if [1], string += [1] round up to quarter hour minutes when not = 0
-
-    console.log(licensingArray);
-    console.log(registrationArray);
-
-    // singular minute/hour for 1, plural for +1 (no abbreviations)
-
-    // < 1 hour: round to the minute
-
-    // > 1 hour: round to the quarter hour
+    // Pass each wait time string (licensing + registration) through transform function.
+    branch.processedLicensing = transformTime(branch.licensing);
+    branch.processedRegistration = transformTime(branch.registration);
 
     console.log(branch);
     return branch;
-  };
+  }
 
-
-  // only show needed info (hide wait time heading if no info)
-
-  // getBranch(xml)
+  /**
+   * Extract the specific branch wait times from xml feed.
+   * @function
+   * @param {xml} xml feed of rmv <branches> wait time data.
+   * @returns {Object.} An object of the requested branch wait time stings for 'licensing' and 'registration'.
+   */
   function getBranch(xml) {
     var location = getLocation();
     var $branch = $(xml).find('branch').filter(function() { return $(this).find('town').text() == location; });
@@ -79,57 +199,54 @@
     };
   }
 
-  // // determine if branch is closed
-  // function isBranchClosed(branch) {
-  //   return ((branch.licensing && branch.licensing == "Closed") && (branch.registration && branch.registration == "Closed"));
-  // }
-
-
-  // handleWaittimes(data)
-  var handleWaittimes = function(data) {
-    var branch = getBranch(data);
-    // if (!isBranchClosed(branch)) {
-      processWaittimes(branch);
-    // }
-  };
-
-  // getData()
-  // Make xmlhttprequest
-  // -- returns <branches> with all <branch> information
-  // -- error handling = "Estimation Unavailable"
-  // Execute callback handleWaittimes() to handle XML
   /**
-   * ex: {
-     town: 'Natick',
-     licensing: '00:07:24' || 'Closed || '00:00:00',
-     registration: '00:00:06' || 'Closed' || '00:00:00'
-   }
+   * Get the rmv wait times for a specific branch.
+   * @function
+   * @returns {Promise.} A promise which contains only the requested branch's wait times on success.
    */
-  function getData() {
-    var promise = $.ajax({
+  function getBranchData() {
+    var promise = $.Deferred(); // promise returned by function
+
+    $.ajax({ // ajax() returns a promise
       type: 'GET',
       url: rmvWaitTimeURL,
+      cache: false,
       dataType: 'xml'
-    });
-
-    promise.done(function(data){
-      handleWaittimes(data);
-    });
-
-    promise.fail(function(){
-      console.log('Estimation unavailable');
-      return 'Estimation unavailable'
+    })
+      .done(function(data){
+        var branch = getBranch(data); // Only send the data for the branch that we need.
+        promise.resolve(branch);
+    })
+      .fail(function(){
+        promise.reject();
     });
 
     return promise;
   }
 
-
+  /**
+   * Gets, transforms, and renders the wait times for a specific rmv branch.
+   * @function
+   */
   function updateTimes() {
-    var data = getData();
-    console.log('getData: ', data);
+    // get the branch data
+    getBranchData()
+      .done(function(branchData){
+      // transform data
+      var branchDisplayData = processWaitTimes(branchData);
+      // render information
+      render(branchDisplayData);
+      el.removeClass('visually-hidden');
+    })
+      .fail(function(){
+        render({
+          processedLicensing: 'Estimation unavailable',
+          processedRegistration: 'Estimation unavailable'
+        });
+        el.removeClass('visually-hidden');
+      });
   }
-
-  // setTimeout every minute to updateTimes()
-  setTimeout(updateTimes, 1000);
+  // Update the wait times once then set interval to update again every minute.
+  updateTimes();
+  var waitTimeRefresh = setInterval(updateTimes, 60000);
 })();
