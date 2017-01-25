96292a85a0721e40166778d0cffd8ff12d55c0c4
Jes Constantine <jesconstantine@gmail.com>
2017-01-25 17:41:38 -0500

    It helps if you tell gulp where to serve from correctly.

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,8 @@ var webserver = require('gulp-webserver');
 * development task
 * Default Task (run when you run 'gulp').
 */
-gulp.task('webserver', function() {
-  gulp.src('/')
+gulp.task('serve', function() {
+  gulp.src('./')
     .pipe(webserver({
       livereload: true,
       open: true
